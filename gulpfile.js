4a6c2539f17c07d98d930b2f59730fdc5b07c5b2
oliba <meschest@gmail.com>
2018-10-09 14:55:31 +0200

    changed comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,10 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
-var sass = require('gulp-sass');
 var postcssPresetEnv = require('postcss-preset-env');
+var sass = require('gulp-sass');
 var cssnano = require('gulp-cssnano');
 var babel = require('gulp-babel');
 var watch = require('gulp-watch');
-
 
 // Laravel Paths
 var paths = {
@@ -43,12 +42,14 @@ gulp.task('css', function() {
         .pipe(gulp.dest(paths.styles.dest))
 });
 
-// in Gulp 4 a function must be provided as second argument, therefore as a workaround a series with just one entry
+// in Gulp 4 a function must be provided as second argument,
+// therefore as a workaround a series with just one entry is used - gulp3 syntax will result in error
 gulp.task('watch',function() {
     gulp.watch(paths.styles.src, gulp.series('css'));
     gulp.watch(paths.scripts.src, gulp.series('js'));
 });
 
 // same here but completely different,
-// in order to let the watch task being run after the default task, to achieve this we need the series() method again
+// the 'watch' task is run before the 'default' task which shouldn't be, gulp requires you to have the default task always run first
+// to achieve this deferral we need the series() method again
 gulp.task('default', gulp.series('watch'));
